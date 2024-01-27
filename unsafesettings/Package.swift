b32b36d4acbd686aec5bff5cf6ba3132e1da3914
Marc Lavergne <mlavergn@gmail.com>
2024-01-27 17:29:15 -0500

    Fix iOS builds

--- unsafesettings/Package.swift
+++ unsafesettings/Package.swift
@@ -1,60 +1,110 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.9
 // Minimum Swift version required
 
 import Foundation
 import PackageDescription
 
-enum Platforms: String {
+enum SDK: String {
     case iphoneos
     case iphonesimulator
     case macosx
     case watchos
 
-    var sdkroot: String {
-        let pipe = Pipe()
-        let task = Process()
-        task.launchPath = "/usr/bin/xcrun"
-        task.arguments = ["--sdk", "\(self.rawValue)", "--show-sdk-path"]
-        task.standardOutput = pipe
-        try! task.run()
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let result = String(data: data, encoding: .utf8) ?? ""
-        task.waitUntilExit()
-        return result
-    }    
+    var description: String {
+        self.rawValue
+    }
+    
+    static var _currentSDK: SDK?
+    var currentSDK: SDK {
+        if let currentSDK = Self._currentSDK {
+            return currentSDK
+        } else {
+            Self._currentSDK = self
+            return self
+        }
+    }
+    
+    var SDKROOT: String {
+        let stdout = Pipe()
+        let process = Process()
+        process.launchPath = "/usr/bin/xcrun"
+        process.arguments = ["--show-sdk-path", "--sdk", "\(self.currentSDK.description)"]
+        process.standardOutput = stdout
+        
+        do {
+            try process.run()
+            process.waitUntilExit()
+        } catch {
+            print(error.localizedDescription)
+            return ""
+        }
+        
+        guard process.terminationStatus == 0 else {
+            return ""
+        }
+        
+        let data = stdout.fileHandleForReading.readDataToEndOfFile()
+        let path = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        return path
+    }
+    
+    var frameworkPath: String {
+        let path = "\(self.SDKROOT)/System/Library/Frameworks"
+        return path
+    }
 }
 
-let SDKROOT = Platforms.iphoneos.sdkroot
-
 var swiftSettings: [SwiftSetting] = [
-    .unsafeFlags([
-        "-Xcc", "-I", "-Xcc", "../include"
-    ]),
+    .unsafeFlags(["-Osize"], .when(configuration: .release)),
+    .unsafeFlags(["-enable-library-evolution"]),
+    .unsafeFlags(
+        [
+            "-Xcc", "-isysroot", "-Xcc", SDK.iphoneos.SDKROOT,
+            "-sdk", SDK.iphoneos.SDKROOT,
+            "-Fsystem", SDK.iphoneos.frameworkPath,
+        ],
+        .when(platforms: [.iOS])
+    ),
+    .unsafeFlags(
+        [
+            "-Xcc", "-isysroot", "-Xcc", SDK.macosx.SDKROOT,
+            "-sdk", SDK.macosx.SDKROOT,
+            "-Fsystem", SDK.macosx.frameworkPath,
+        ],
+        .when(platforms: [.macOS])
+    ),
 ]
 
 var cxxSettings: [CXXSetting] = [
-    .headerSearchPath("/System/Library/Frameworks"),
     .unsafeFlags(["-fvisibility=hidden"]),
 ]
 
 var cSettings: [CSetting] = [
-    .unsafeFlags([
-        "-isysroot", SDKROOT,
-        "-iframework", "\(SDKROOT)/System/Library/Frameworks"
-    ])
+    .define("ONLY_ACTIVE_ARCH", to: "YES", .when(configuration: .debug)),
+    .unsafeFlags(
+        [
+            "-isysroot", SDK.iphoneos.SDKROOT,
+        ],
+        .when(platforms: [.iOS])
+    ),
+    .unsafeFlags(
+        [
+            "-isysroot", SDK.macosx.SDKROOT,
+        ],
+        .when(platforms: [.macOS])
+    )
 ]
 
 var linkerSettings: [LinkerSetting] = [
-    .unsafeFlags(["-F", "/System/Library/Frameworks"]),
-    .linkedFramework("CoreData", nil),
-    .linkedFramework("Foundation", nil),
+    .linkedFramework("Foundation"),
+    .linkedFramework("CoreData"),
 ]
 
 let package = Package(
     name: "Demo",
-    platforms: [.iOS("17.0"), .macOS("14.0"), .watchOS("10.0")],
+    platforms: [.iOS("17.0"), .macOS("13.0"), .watchOS("10.0")],
     products: [
-        .library(name: "Demo", targets: ["Demo"])
+        .library(name: "Demo" ,targets: ["Demo"])
     ],
     targets: [
         .target(
@@ -63,6 +113,7 @@ let package = Package(
             path: "Sources",
             exclude: ["Demo.xcdatamodeld", "Info.plist"],
             resources: [ .copy("Info.plist") ],
+            cSettings: cSettings,
             swiftSettings: swiftSettings,
             linkerSettings: linkerSettings
         ),
